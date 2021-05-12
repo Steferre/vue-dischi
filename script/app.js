82d581def79118a2c257a0e484c9cb71cda9f13b
Steferre <ferrera.ste@gmail.com>
2021-05-12 17:34:50 +0200

    Aggiunto bonus 2

--- script/app.js
+++ script/app.js
@@ -13,12 +13,31 @@ const app = new Vue({
 
     },
     methods: {
-        getMusicAlbums() { 
+        getMusicAlbums() {
+            // definisco una variabile list alla quale assegnero' l'oggetto ottenuto come risposta dopo la chiamata al server
+            let list;
+            
+            // effettuo la chiamata per ottenere la lista di dischi
             axios.get('https://flynn.boolean.careers/exercises/api/array/music')
                 .then((arrayObj) => {
-                    this.musicAlbums = arrayObj.data.response
-                    console.log(this.musicAlbums);  
-                })
+                    list = arrayObj.data.response
+                    console.log(list) // lista come arrivata dal server
+                    /*
+                    PER RISOLVERE IL BONUS 2
+                    Una volta trovata la lista dei dischi (array di oggetti)
+                    applico la funzione sort dove come argomento vine passata un'altra funzione
+                    x indica il primo valore e y il valore di confronto ed in base alla funziine di default di .sort
+                    l'array viene ordinato in modo crescente
+                    */
+                    list.sort(function (x, y) {
+                        return x.year - y.year
+                    })
+                    console.log(list) // lista riordinata in modo crescente in base all'anno di produzione del disco
+
+                    // assegno alla mia chiave musicAlbums definita nei data, il valore list 
+                    this.musicAlbums = list;
+
+                })    
         }
     },
 })
